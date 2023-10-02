029fd112030bd7491675d4a421734b389804f5c2
Harry Papaxenopoulos <harry@argonautsecurity.com>
2023-10-02 20:23:30 +0300

    Add csharpier and codeium

--- plugins/user.lua
+++ plugins/user.lua
@@ -19,4 +19,15 @@ return {
       { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
     },
   },
+  {
+    "Exafunction/codeium.vim",
+    event = "Bufenter",
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+      vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+      vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+    end,
+  },
 }
