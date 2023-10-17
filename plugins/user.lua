7ae52aad5496f3637b4f0cf2ae6840952223bafe
Harry Papaxenopoulos <hpapaxen@gmail.com>
2023-10-17 08:26:45 +0300

    Add testing functionality

--- plugins/user.lua
+++ plugins/user.lua
@@ -48,6 +48,7 @@ return {
           end,
         },
       }, neotest_ns)
+      ---@diagnostic disable-next-line: missing-fields
       require("neotest").setup({
         adapters = {
           require("neotest-go"),
@@ -57,7 +58,21 @@ return {
     keys = {
       { "<leader>T", "", desc = "Run Tests"},
       { "<leader>Tt", "<cmd>lua require('neotest').run.run()<cr>", desc = "Run Current Test" },
+      { "<leader>TA", "<cmd>lua require('neotest').run.run(vim.fn.getcwd())<cr>", desc = "Run All Tests"},
       { "<leader>Tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", desc = "Test Current File" },
+      { "<leader>Td", "<cmd>lua require('neotest').run.run({ strategy = 'dap' })<cr>", desc = "Debug Test Current File" },
+      { "<leader>Ts" , "<cmd>lua require('neotest').summary.toggle()<cr>", desc = "Toggle Summary" },
+      { "<leader>Tj", "<cmd>lua require('neotest').jump.next({ status = 'failed' })<cr>", desc = "Next Failed Test" },
+      { "<leader>TJ", "<cmd>lua require('neotest').jump.prev({ status = 'failed' })<cr>", desc = "Previous Failed Test" },
+      { "<leader>Tn", "<cmd>lua require('neotest').jump.next({ status = 'running' })<cr>", desc = "Next Running Test" },
+      { "<leader>TN", "<cmd>lua require('neotest').jump.prev({ status = 'running' })<cr>", desc = "Previous Running Test" },
+      { "<leader>Tp", "<cmd>lua require('neotest').jump.next({ status = 'passed' })<cr>", desc = "Next Passed Test" },
+      { "<leader>TP", "<cmd>lua require('neotest').jump.prev({ status = 'passed' })<cr>", desc = "Previous Passed Test" },
+      { "<leader>To", "<cmd>lua require('neotest').output.open({ enter = true, auto_close = true })<cr>", desc = "Open Test Output" },
+      { "<leader>Tl", "<cmd>lua require('neotest').run.run_last()<cr>", desc = "Run Last Test" },
+      { "<leader>Tf", "<cmd>lua require('neotest').run.run_last({ strategy = 'dap' })<cr>", desc = "Debug Last Test" },
+      { "<leader>Te", "<cmd>lua require('neotest').run.stop()<cr>", desc = "Stop Test" },
+      { "<leader>Ta", "<cmd>lua require('neotest').run.attach()<cr>", desc = "Attach Test" },
     }
   }
 }
